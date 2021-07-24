e4b3fc382fb694c26c19fa009a7365fc60c46764
faisal <faisal@w3engineers.com>
2021-07-25 00:33:56 +0600

    Improvement on full file

--- app.js
+++ app.js
@@ -1,38 +1,68 @@
-/* form validation*/
+const firstError = document.getElementById("first-error")
+const lastError = document.getElementById("last-error")
+const emailError = document.getElementById("email-error")
+const passwordError = document.getElementById("password-error")
+const form = document.getElementById("form")
+const email = document.getElementById("email")
+const inputs = Array.from(document.getElementsByTagName("input"))
 
-var exclamation = document.getElementsByClassName("error");
-var form = document.getElementById("form");
-var firstName = document.getElementById("firstName");
-var lastName = document.getElementById("lastName");
-var email = document.getElementById("email");
-var password = document.getElementById("password");
-var formError = document.getElementsByClassName("form_error");
-var input = document.getElementsByTagName("input");
+const firstTE = document.getElementById("first-text-error");
+const lastTE = document.getElementById("last-text-error");
+const emailTE = document.getElementById("email-text-error");
+const passwordTE = document.getElementById("password-text-error");
 
-/* validation*/
-  function formValidation(event) {
-    /*for loop for all input fields except submit input, so it is 4*/
-    for (i = 0; i < 4; i++){
-      /*clear the form*/
-      input[i].classList.remove("error");
-      formError[i].style.display = "none";
-      
-      /*email validation*/
-      if (!email.validity.valid) {
-        formError[2].style.display = "block";
-        email.classList.add("error");
-        event.preventDefault();
-      }
 
-      /*validation of other fields*/
-      if((input.type != "email" || input.type != "submit") && input[i].value == ""){
-      formError[i].style.display = "block";
-      input[i].classList.add("error");
-      event.preventDefault();
-      }
+form.addEventListener("submit" , (e) => {
+    e.preventDefault();
+    let errorCount = 0;
+    if (inputs[0].value.length < 1) {
+         firstTE.classList.remove("error");
+    firstError.classList.remove("error");
+    errorCount +=1
+    } else {
+firstTE.classList.add("error");
+    firstError.classList.add("error");
     }
+   if (inputs[1].value.length < 1) {
+        lastTE.classList.remove("error");
+         lastError.classList.remove("error");
+         errorCount +=1
+    } else {
+            lastTE.classList.add("error");
+         lastError.classList.add("error");
+    }
+    if (!inputs[2].value.match(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)) {
+        emailTE.classList.remove("error");
+        emailError.classList.remove("error");
+        inputs[2].value = ""
+        inputs[2].placeholder = "email@example/com"
+        errorCount +=1
+    } else {
+        emailTE.classList.add("error");
+        emailError.classList.add("error");
+    }
+   
+     if (inputs[3].value.length < 1) {
+        passwordTE.classList.remove("error");
+        passwordError.classList.remove("error");
+        errorCount +=1
+    } else {
+        passwordTE.classList.add("error");
+        passwordError.classList.add("error")
+    }
+   
 
-  }
+  
+    if (errorCount == 0) {
+        alert(`Congrats your submission was accepted
+        name: ${inputs[0].value} ${inputs[1].value}
+        email: ${inputs[2].value}
+        password: ***hidden***`)
 
-  /*event listener on button, call the formValidation function*/
-  form.addEventListener("submit", formValidation, false)
+        inputs.forEach((e) => e.value = "" )
+        
+    }
+    
+   
+    
+})
